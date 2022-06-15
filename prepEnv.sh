4d991d4f7240db63d79c80b4ba6cb1eb182d7669
Ian O'Dwyer <iodwyer@kx.com>
2022-06-15 18:14:28 -0400

    update file names

--- prepEnv.sh
+++ prepEnv.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source .env
 source .cloud_auth_env 
-eval "echo \"$(cat cfg/assembly_sample.yaml)\"" > cfg/resolved_assembly_sample.yaml
+eval "echo \"$(cat cfg/assembly.yaml)\"" > cfg/resolved_assembly.yaml
